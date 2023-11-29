53c8beb65a7bf7b3374b5109af21d00291f2484b
S-M-Khan <momodouseringkhan@gmail.com>
2023-11-29 21:58:15 +0000

    another commit

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-console.log("Host:", process.env.MYSQLHOST);
-console.log("User:", process.env.MYSQLUSER);
-
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
