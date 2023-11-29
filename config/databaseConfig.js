fc579a721ac7a42a4d5cc4513dfa049200c9b1de
S-M-Khan <momodouseringkhan@gmail.com>
2023-11-29 23:53:12 +0000

    commit

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT,
 });
 
