49f46f6c55405327ee0db0c25655b7bbf3f0c168
S-M-Khan <momodouseringkhan@gmail.com>
2023-11-30 00:34:00 +0000

    yet another commit

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST || "localhost",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "",
+    database: process.env.MYSQLDATABASE || "books",
+    port: process.env.MYSQLPORT || 3306,
 });
 
 module.exports = pool;
